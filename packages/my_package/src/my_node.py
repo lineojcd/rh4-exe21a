2f54bbebd416558f67162d5ea5ebb3fb87133ae5
lineojcd <45730495+lineojcd@users.noreply.github.com>
2020-11-03 22:59:23 +0100

    Add files via upload

--- packages/my_package/src/my_node.py
+++ packages/my_package/src/my_node.py
@@ -2,12 +2,14 @@
 
 import os
 import rospy
-from duckietown import DTROS
-#from std_msgs.msg import String
+from duckietown.dtros import DTROS,NodeType
+from std_msgs.msg import String
 from sensor_msgs.msg import CompressedImage
-import picamera
-import picamera.array
+
+# import picamera
+# import picamera.array
 #import cv2_to_compressed_imgmsg
+import cv2
 import numpy as np
 from cv_bridge import CvBridge
 
@@ -15,9 +17,9 @@ class MyNode(DTROS):
 
     def __init__(self, node_name):
         # initialize the DTROS parent class
-        super(MyNode, self).__init__(node_name=node_name)
+        super(MyNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
         # construct publisher
-        self.pub = rospy.Publisher('/duckiemon/camera_node/image/compressed', CompressedImage, queue_size=10)
+        self.pub = rospy.Publisher('/jcdgo/camera_node/image/compressed', CompressedImage, queue_size=10)
 
         
     def run(self):
@@ -25,27 +27,36 @@ class MyNode(DTROS):
         rate = rospy.Rate(30) # 200Hz
         width = 640
         height = 480
-       
+        
+        cap = cv2.VideoCapture(2)
 
-        with picamera.PiCamera() as camera:
-            camera.resolution = (width, height)
+        while not rospy.is_shutdown():
+            # Capture frame-by-frame
+            # cap.read() read the video by frame
+            # return ret and frame:
+            # ret is bool type, get true if read successful, otherwise false
+            # frame: 3D matrix,  the everyframe you read
+            ret, frame = cap.read()
 
-            while not rospy.is_shutdown():
-                with picamera.array.PiRGBArray(camera) as output:
-                    camera.capture(output, 'bgr')
-                    output = output.array
-                    #red = output[:,:,0]
-                    #output[:,:,0] = output[:,:,2]
-                    #output[:,:,2] = red
-                    compressed_img_msg = br.cv2_to_compressed_imgmsg(output, dst_format='jpg')
-                    rospy.loginfo("Publishing image from Picamera")
-                    self.pub.publish(compressed_img_msg)
-                    rate.sleep()
+            # you can now treat output as a normal numpy array
+            if ret:
+                # print("frame.shape",frame.shape)
+                compressed_img_msg = br.cv2_to_compressed_imgmsg(frame, dst_format='jpg')
+                rospy.loginfo("Publishing image from Duckiebot camera")
+                self.pub.publish(compressed_img_msg)
+            else:
+                rospy.loginfo("No image published from Duckiebot camera")
+
+            rate.sleep()
+
+
 
 if __name__ == '__main__':
     # create the node
     br = CvBridge()
-    node = MyNode(node_name='my_node')
+    print("launching node..")
+    node = MyNode(node_name='jcdgo_node')
+    
     # run node
     node.run()
     # keep spinning
